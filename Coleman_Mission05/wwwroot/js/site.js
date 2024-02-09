b4ad2a80e58ccd45570c85b41feaf745867b505d
cdawg1213 <103866953+cdawg1213@users.noreply.github.com>
2024-02-08 21:20:27 -0700

    Add project files.

---
+++ Coleman_Mission05/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(document).ready(function () {
+    $('#calculateBtn').click(function () {
+        var hours = parseFloat($('#hours').val());
+
+        // Check if the input is a positive number
+        if (isNaN(hours) || hours < 0) {
+            alert('Please enter a positive number for the hours.');
+            return;
+        }
+
+        var rate = parseFloat($('#rate').val().replace('$', ''));
+        var total = hours * rate;
+        $('#total').val('$' + total.toFixed(2));
+    });
+});
